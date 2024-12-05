0d99b4bbfb8056403ad9092d500c029cd7211927
gmaheswaranmca <gmaheswaranmca@gmail.com>
2024-12-05 16:41:12 +0530

    c++

--- phase1/learnings/Day22/cpp/prg03.cpp
+++ phase1/learnings/Day22/cpp/prg03.cpp
@@ -5,6 +5,10 @@ using namespace std;
 
 int main() {
     int id = 100;
+    int* ptrId = &id;
     cout << "id=" << id << endl;
+    cout << "addr of id=" << &id << endl;
+    cout << "addr of id=" << ptrId << endl;
+    cout << "id=" << *ptrId << endl;
     return 0;
 }
